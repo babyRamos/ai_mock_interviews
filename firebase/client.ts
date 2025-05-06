c52c32ccfba929f220dff2d81d2ef1eca125ebfb
Pengyu Song <song0150@algonquinlive.com>
2025-05-05 21:39:59 -0400

    Update .gitignore and firebase client configuration

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,16 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDCwjPQdntA8QywyIAPSROVoPmyQTiuCBI",
-    authDomain: "prepwise-b2462.firebaseapp.com",
-    projectId: "prepwise-b2462",
-    storageBucket: "prepwise-b2462.firebasestorage.app",
-    messagingSenderId: "343901868310",
-    appId: "1:343901868310:web:ae83ad1bf8c0e58ab48037",
-    measurementId: "G-X54C52YBMN"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
